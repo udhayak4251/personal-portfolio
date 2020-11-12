419018307eb111506ea453e03222c6f34af59c48
Udhay <udhayak4251@gmail.com>
2020-11-12 23:32:36 +0530

    latest

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-gsap.fromTo("#letterU", {y: -700}, {y:0, duration: 1.5});
-gsap.fromTo("#letterK", {y: 700}, {y:0, duration: 1.5, delay: 0.5});
-gsap.fromTo("#letterK", {y: 700}, {y:0, duration: 1.5, delay: 0.5});
+gsap.fromTo("#letterU", {x: -700}, {x:0, duration: 1.5});
+gsap.fromTo("#letterK", {x: 700}, {x:0, duration: 1, delay: 0.5});
 gsap.fromTo("#intro", {display: "flex", opacity: 1}, {display: "none", opacity: 0, duration: 1, delay: 2});
-gsap.fromTo("#main", {display: "none"}, {display: "block", duration: 4});
+gsap.fromTo("#main", {display: "none"}, {display: "flex", duration: 4});
 tl.fromTo("#content", {x: -100},{x: 0, ease: "back.out(2.5)", duration: 1.5, delay: 3});
 tl.fromTo("#homeImage", {x: 100},{x: 0, ease: "back.out(2.5)", duration: 1.5, delay: -1.5});
 const showMenu = (toggleId, navid, menuId) => {
